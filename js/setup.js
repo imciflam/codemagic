e22cf266a0d9b9fc3260fe855f67539a6d5ccc4b
imciflam <33329377+imciflam@users.noreply.github.com>
2018-09-10 22:32:06 +0300

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,7 @@ var ENTER_KEYCODE = 13;
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
-var WIZARD_NAMES = ['Варянус', 'Машанус', 'Морэльный', 'Олегус', 'Витэльнэ']
+var WIZARD_NAMES = ['Варянус', 'Машанус', 'Морэльный', 'Олегус', 'Витэльный']
 var WIZARD_SURNAMES = ['Юзанус', 'Казус', 'Саллянус', 'Павукан']
 
 
