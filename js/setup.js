53353b808ca93512c4c73ce2ad0fe83b4caff3cf
imciflam <33329377+imciflam@users.noreply.github.com>
2018-09-12 09:52:46 +0300

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,12 @@
 'use strict';
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
-var WIZARD_NAMES = ['Варянус', 'Машанус', 'Морэльный', 'Олегус', 'Витэльный']
-var WIZARD_SURNAMES = ['Юзанус', 'Казус', 'Саллянус', 'Павукан']
+//var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+//var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+//var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
 
+var WIZARD_NAMES = ['Варянус', 'Машанус', 'Морэльный', 'Олегус']
+var WIZARD_SURNAMES = ['Юзанус', 'Казус', 'Саллянус', 'Павукан']
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -14,7 +14,6 @@ document.querySelector('.setup-similar').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 .content;
-
 
 
 var getRandomInteger = function (min, max) {
@@ -26,11 +25,7 @@ var wizards =
 {   
 	name: WIZARD_NAMES[getRandomInteger(0, WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomInteger(0, WIZARD_SURNAMES.length)],
 	coatColor: '#'+(Math.random()*0xFFFFFF<<0).toString(16),
-	eyesColor: 'rgb(348,100,90)'
-},
-{
-	name: WIZARD_NAMES[getRandomInteger(0, WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomInteger(0, WIZARD_SURNAMES.length)],
-	coatColor: '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+	eyesColor: 'rgb(348,100,90)', 
 },
 {
 	name: WIZARD_NAMES[getRandomInteger(0, WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomInteger(0, WIZARD_SURNAMES.length)],
@@ -38,7 +33,11 @@ var wizards =
 },
 {
 	name: WIZARD_NAMES[getRandomInteger(0, WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomInteger(0, WIZARD_SURNAMES.length)],
-	coatColor: '#'+(Math.random()*0xFFFFFF<<0).toString(16),
+	coatColor: '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+},   
+{
+	name: WIZARD_NAMES[getRandomInteger(0, WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomInteger(0, WIZARD_SURNAMES.length)],
+	coatColor: 'rgb(0, 51, 255)',
 	eyesColor: 'rgb(0, 204, 51)'
 }
 ]
