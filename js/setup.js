ce4b444545994c4947a0dbb8a39a51b3614c237c
Vita <vitalia@myrambler.ru>
2018-10-01 12:33:50 +0300

    /

--- js/setup.js
+++ js/setup.js
@@ -17,7 +17,7 @@ var userDialogOpen = document.querySelector('.setup-open');
 var userDialogClose = document.querySelector('.setup-close');
 var coatColoring = document.querySelector('.setup-wizard .wizard-coat');
 var eyesColoring = document.querySelector('.setup-wizard .wizard-eyes');
-var fireballColoring = document.querySelector('.setup-fireball-wrap');
+var fireballColoring = document.querySelector('.setup-fireball-wrap'); 
 
 var openPopup = function() 
 {
@@ -124,3 +124,46 @@ for (var i=0; i<wizards.length; i++)
 	
 	similarListElement.appendChild(wizardElement);
 }
+
+
+var dialogHandle = userDialog.querySelector('.upload');
+dialogHandle.addEventListener('mousedown', function (evt)
+{ 	
+  evt.preventDefault(); 
+  //default coordinates
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+    //document.addEventListener("mousemove", onMouseMove);
+    //document.addEventListener("mouseup", onMouseUp);
+    var onMouseMove = function (moveEvt)
+    {
+    moveEvt.preventDefault();
+    //find cursor's displacement during one event
+    //the difference between start coords and current coords
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+    //rewrite start location
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+    //add the difference thus moving the pin on cursor's place
+    userDialog.style.top = (userDialog.offsetTop - shift.y) + "px";
+    userDialog.style.left = (userDialog.offsetLeft - shift.x) + "px";
+    };
+
+     var onMouseUp = function (upEvt)
+    {
+      upEvt.preventDefault();
+       document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    }
+
+     document.addEventListener("mousemove", onMouseMove);
+     document.addEventListener("mouseup", onMouseUp);
+});
