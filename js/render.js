5fc1d5007cd64a56c1294927fb2343ca80df9868
Vita <vitalia@myrambler.ru>
2018-10-15 13:42:47 +0300

    new module

---
+++ js/render.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+
+	var similarWizardTemplate = document.querySelector("#similar-wizard-template").content;
+		
+	var renderWizard = function(wizard)
+	{
+		var wizardElement = similarWizardTemplate.cloneNode(true);
+		wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+		wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+		wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes; 
+		return wizardElement;
+	}
+	var similarListElement = document.querySelector(".setup-similar-list");
+	window.load(function(wizards)
+	{
+	var fragment = document.createDocumentFragment();
+	for (var i=0; i<4; i++)
+	{
+		fragment.appendChild(renderWizard(wizards[i]));
+	}
+	similarListElement.appendChild(fragment);
+	document.querySelector(".setup-similar").classList.remove("hidden");
+	});
+
+})();
--- js/setup.js
+++ js/setup.js
@@ -102,31 +102,7 @@ var getRandomInteger = function (min, max) {
 
 
 //render
-(function () {
-
-	var similarWizardTemplate = document.querySelector("#similar-wizard-template").content;
-		
-	var renderWizard = function(wizard)
-	{
-		var wizardElement = similarWizardTemplate.cloneNode(true);
-		wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-		wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
-		wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes; 
-		return wizardElement;
-	}
-	var similarListElement = userDialog.querySelector(".setup-similar-list");
-	window.load(function(wizards)
-	{
-	var fragment = document.createDocumentFragment();
-	for (var i=0; i<4; i++)
-	{
-		fragment.appendChild(renderWizard(wizards[i]));
-	}
-	similarListElement.appendChild(fragment);
-	userDialog.querySelector(".setup-similar").classList.remove("hidden");
-	});
-
-})();
+ 
 
 //setup.js
 
